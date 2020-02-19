59562250773074d66ed4b6266690f8d431ef7ca3
Danielle Marie <dhutchings36@yahoo.com>
2020-02-18 18:07:04 -0600

    fixed private route

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import TokenService from '../services/token-service';
+// import App from './App/App';
 
-function PrivateRoute({ component, ...props }) {
-  const Component = component
+function PrivateRoute({ render:Render, ...props }) {
+  // const Component = component
+  // console.log(component)
   return (
     <Route
       {...props}
       render={componentProps => (
         TokenService.hasAuthToken()
-          ? <Component {...componentProps} />
+          ? Render(componentProps)
           : <Redirect
               to={{
                 pathname: '/login',
