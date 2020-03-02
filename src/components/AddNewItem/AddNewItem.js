9fbb4fcf2c948db7b13b9198ff69c1eb53e68aa7
Danielle Marie <dhutchings36@yahoo.com>
2020-03-01 19:12:14 -0600

    working on add new item

--- src/components/AddNewItem/AddNewItem.js
+++ src/components/AddNewItem/AddNewItem.js
@@ -1,31 +1,34 @@
 import React from 'react';
 import AppContext from '../../contexts/AppContext';
 
-// function handleClick() {
-//   textInput.current.focus()
-// }
 
 class AddNewItem extends React.Component {
+  constructor(props) {
+    super(props)
+    this.textInput = React.createRef()
+  }
+
+  handleClick() {
+    this.textInput.current.focus()
+  }
+
 
   static contextType = AppContext;
 
   render() {
-    const textInput = React.createRef()
-    function handleClick() {
-      textInput.current.focus()
-    }
+
     return (
       <div className='add-new-item'>
         <h2>Add New Item</h2>
         <div className='new-item-link'>
           <label htmlFor='item-link'>Item Url</label>
-          <input type='url' name='item-link' id='item-url' ref={textInput} required />
+          <input type='url' name='item-link' id='item-url' ref={this.textInput} required />
         </div>
         <button
           className='add-new-item-button'
           id='add-new-item-link'
           onClick={
-            () => { this.props.handleAddNewItem(handleClick()) }
+            () => { this.props.handleAddNewItem(this.handleClick()) }
           }
         >
           Add to List
